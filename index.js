07aa9692d7e4335fca2f9cb9f38063366afdb047
kanhaiya <kanhaiyas@lambdatest.com>
2019-03-26 16:37:02 +0530

    tunnel v.0 for getting tunnelId of running tunnel

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
 var LambdatestTunnel = require('./lib/Tunnel');
 
 var tunnel = new LambdatestTunnel();
-tunnel.start({ key: 'jbhUoF5lLRIjAN9DGp4jtoQZaGCyLUXEelRf76m4ZVbrKR3KFo', user:'kanhaiya1501@gmail.com' }, function(e) {
+tunnel.start({ key: 'jbhUoF5lLRIjAN9DGp4jtoQZaGCyLUXEelRf76m4ZVbrKR3KFo', user:'kanhaiya1501@gmail.com' }, function(e, status) {
   if(e) throw e;
-  console.log("Started LambdatestTunnel");
+  console.log("Started LambdatestTunnel Status" + status);
 })
+
+// var getPort = require('get-port');
+ 
+// getPort(function (err, port) {
+//   console.log(port);
+//   //=> 51402
+// });
